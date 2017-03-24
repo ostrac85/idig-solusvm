3e8b546a8bd58ece930cdc3c457168eb6754380a
ostrac85 <ostrac85@gmail.com>
2017-03-24 15:27:35 +0800

    solusvm

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.Client = require('./lib/solus/client.js');
+exports.Vm = require('./lib/solus/VirtualMachine');
+exports.Reseller = require('./lib/solus/Reseller');
+exports.NodeAction = require('./lib/solus/Nodes');
